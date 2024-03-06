2324946338f63151bef724e77711228387b9dac2
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-06 18:22:52 -0300

    Adding the string function

--- staff/models.py
+++ staff/models.py
@@ -5,16 +5,25 @@ class Cargos(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nome
+
 
 class Estados(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     sigla = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.name
+
 
 class Linguagens(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
 
 
 class Funcionarios(models.Model):
@@ -26,7 +35,7 @@ class Funcionarios(models.Model):
     estado = models.ForeignKey(Estados, on_delete=models.PROTECT)
     cargo = models.ManyToManyField(Cargos)
     salario = models.FloatField()
-    linguagem = models.ManyToManyField(Linguagens, limit_choices_to=5)
+    linguagem = models.ManyToManyField(Linguagens)
 
 
 
