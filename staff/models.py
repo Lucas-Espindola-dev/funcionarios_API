edba3a24dfcc2874ef1ffb8b19d15cb5523b3031
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-06 14:48:23 -0300

    Creating Models

--- staff/models.py
+++ staff/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 
-class
+class Funcionarios(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    idade = models.CharField(max_length=15)
+    email = models.EmailField
+    cargo = models.CharField(max_length=200)
+    salario = models.FloatField
+
+
