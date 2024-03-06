fdf40c3525e2dea8f239fcddc66023cd054eefc7
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-06 14:52:36 -0300

    Adding new fields to Class Funcionarios

--- staff/models.py
+++ staff/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class Funcionarios(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
-    idade = models.CharField(max_length=15)
+    idade = models.IntegerField
     email = models.EmailField
+    celular = models.CharField(max_length=15)
+    estado = models.CharField(max_length=30)
     cargo = models.CharField(max_length=200)
     salario = models.FloatField
-
 
