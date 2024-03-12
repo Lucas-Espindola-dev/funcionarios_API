736f76fc2eeec71fbc8c981adb6ed5ee4b30125c
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-12 15:49:23 -0300

    Correcting Rout

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from staff.views import FuncionariosViewSet
 
 router = routers.DefaultRouter()
-router.register('funcionarios')
+router.register('funcionarios', FuncionariosViewSet, basename='funcionarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
