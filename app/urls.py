f22d9f2008615ce3165b684aaa56a8d44716ca23
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-12 15:36:07 -0300

    Creating the urls routs

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('funcionarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('staff.urls'))
+    path('', include(router.urls))
 ]
