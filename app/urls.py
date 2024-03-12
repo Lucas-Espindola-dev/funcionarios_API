ba3c7a56934a5e0e1eca19f0d3a1b8706cc116b2
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-12 16:01:46 -0300

    Adding new Routs

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from staff.views import FuncionariosViewSet
+from staff.views import FuncionariosViewSet, CargosViewset, LinguagensViewSet
 
 router = routers.DefaultRouter()
 router.register('funcionarios', FuncionariosViewSet, basename='funcionarios')
+router.register('cargos', CargosViewset, basename='cargos')
+router.register('linguagens', LinguagensViewSet, basename='linguagens')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
