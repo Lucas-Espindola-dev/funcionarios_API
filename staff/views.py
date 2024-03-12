caeca382d7c3f669fc61f27cb429f42c01b87327
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-12 15:57:14 -0300

    Creating a Class Viewset to Cargos

--- staff/views.py
+++ staff/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from staff.models import Funcionarios
-from staff.serializers import FuncionariosSerializer
+from staff.models import Funcionarios, Cargos
+from staff.serializers import FuncionariosSerializer, CargosSerializer
 from rest_framework import viewsets
 
 
 class FuncionariosViewSet(viewsets.ModelViewSet):
     queryset = Funcionarios.objects.all()
     serializer_class = FuncionariosSerializer
+
+
+class CargosViewset(viewsets.ModelViewSet):
+    queryset = Cargos.objects.all()
+    serializer_class = CargosSerializer
