e0fe01ff7a1325c9b9e04e319bc510a6b72abd8e
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-13 16:03:29 -0300

    Structuring view completly

--- staff/views.py
+++ staff/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Avg, Count
 from staff.models import Funcionarios, Cargos, Linguagens
 from staff.serializers import FuncionariosSerializer, CargosSerializer, LingugensSerializer
-from rest_framework import viewsets, views, response
+from rest_framework import viewsets, views, response, status
 
 
 class FuncionariosViewSet(viewsets.ModelViewSet):
@@ -22,4 +23,14 @@ class FuncionariosStats(views.APIView):
     queryset = Funcionarios.objects.all()
 
     def get(self, request):
-        ...
+        total_funcionarios = self.queryset.count()
+        funcionarios_por_cargo = self.queryset.values('cargo__nome').annotate(count=Count('id'))
+        funcionarios_por_linguagem = self.queryset.values('linguagem__nome').annotate(count=Count('id'))
+
+        return response.Response(
+            data={'total_funcionarios': total_funcionarios,
+                  'funcionarios_por_cargo': funcionarios_por_cargo,
+                  'funcionarios_por_linguagem': funcionarios_por_linguagem,
+                  },
+            status=status.HTTP_200_OK
+        )
