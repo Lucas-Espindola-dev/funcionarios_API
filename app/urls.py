4021ce56a35e98a3221a3f120d120ad9a630279e
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-28 13:22:20 -0300

    Creating a router for Senioridade

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from staff.views import FuncionariosViewSet, CargosViewset, LinguagensViewSet, FuncionariosStats
+from staff.views import FuncionariosViewSet, CargosViewset, LinguagensViewSet, FuncionariosStats, SenioridadeViewSet
 
 router = routers.DefaultRouter()
 router.register('funcionarios', FuncionariosViewSet, basename='funcionarios')
 router.register('cargos', CargosViewset, basename='cargos')
 router.register('linguagens', LinguagensViewSet, basename='linguagens')
+router.register('senioridade', SenioridadeViewSet, basename='senioridade')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
