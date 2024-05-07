9d5b7c157d451bb108d84c3fd93aac43bcf73f68
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-07 12:34:33 -0300

    Registering rout for stats

--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from staff.views import FuncionariosViewSet, CargosViewset, LinguagensViewSet, FuncionariosStats, SenioridadeViewSet
+from staff.views import FuncionariosViewSet, CargosViewset, LinguagensViewSet, FuncionariosStatsViewSet, SenioridadeViewSet
 
 router = routers.DefaultRouter()
 router.register('funcionarios', FuncionariosViewSet, basename='funcionarios')
 router.register('cargos', CargosViewset, basename='cargos')
 router.register('linguagens', LinguagensViewSet, basename='linguagens')
 router.register('senioridade', SenioridadeViewSet, basename='senioridade')
+router.register('dados', FuncionariosStatsViewSet, basename='funcionarios-stats')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('dados/', FuncionariosStats.as_view(), name='funcionarios-stats')
 ]
