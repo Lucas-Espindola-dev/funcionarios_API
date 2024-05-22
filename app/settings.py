fa9bc7562dedc1d10b9991462743be7e3e40f3db
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-22 08:37:01 -0300

    Adding the configuration from simplejwt to restframework

--- app/settings.py
+++ app/settings.py
@@ -133,4 +133,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
